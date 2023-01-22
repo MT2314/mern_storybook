1b8766dc584e09b5d69078c9f752e68aab51eabf
Mike Tkachuk <tkachuk2314@gmail.com>
2023-01-22 17:58:03 -0500

    heroku prep

--- server.js
+++ server.js
@@ -16,3 +16,15 @@ app.listen(port, () => {
   });
   console.log(`Server is running on port: ${port}`);
 });
+
+// ... other imports
+const path = require("path");
+
+// ... other app.use middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
